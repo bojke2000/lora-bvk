6d127b9abc640724497c0c78c8786abc94b4d72e
MBP2017 <mbp2017@MBP2017s-MacBook-Pro.local>
2023-07-26 16:19:51 +0200

    added new service mqttservice

---
+++ src/main/java/com/bojan/lora/service/MqttService.java
@@ -0,0 +1,48 @@
+package com.bojan.lora.service;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+public class MqttService {
+
+    private MqttClient mqttClient;
+    private String brokerUrl = "tcp://mqtt.eclipse.org:1883"; // Replace with your MQTT broker URL
+    private String topic = "test/topic"; // Replace with the topic you want to publish to
+    private String message = "Hello, MQTT!"; // Replace with the message you want to publish
+
+    public MqttService() {
+        String clientId = MqttClient.generateClientId();
+
+        try {
+            mqttClient = new MqttClient(brokerUrl, clientId);
+            MqttConnectOptions connectOptions = new MqttConnectOptions();
+            connectOptions.setCleanSession(true);
+
+            mqttClient.connect(connectOptions);
+
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publish(String message) {
+        try {
+            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+            mqttClient.publish(topic, mqttMessage);
+
+            System.out.println("Message published: " + message);
+        } catch (Exception e) {
+        }
+    }
+
+    public void stop() {
+        try {
+            if (mqttClient != null) {
+                mqttClient.disconnect();
+                mqttClient.close();
+            }
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
